ca06cc9c043849ae6e0a8b63bcb8b84afea0ce2f
Niku Grönberg <gronbergniku@gmail.com>
2022-06-15 20:55:16 +0300

    move icon to separate file

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, provide } from "vue";
 import type { Image } from "../types";
+import XIcon from "./XIcon.vue";
 
 export interface ImageViewProps {
   setImage: (image: Image) => void;
@@ -33,18 +34,7 @@ const closeImageViewer = () => {
         <div
           class="absolute top-0 right-0 text-xl cursor-pointer hover:text-text-light-hover text-glow"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-8 w-8"
-            viewBox="0 0 18 18"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <XIcon />
         </div>
       </div>
     </div>
